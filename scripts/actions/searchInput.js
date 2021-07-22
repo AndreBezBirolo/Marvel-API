7f03f8d54cf43ffca1ca1beda7f60d687e2f32cd
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-07-22 19:40:52 -0300

    Validation empty input

--- scripts/actions/searchInput.js
+++ scripts/actions/searchInput.js
@@ -2,9 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search');
     searchInput.onkeyup = (e) => {
         let valueSearch = searchInput.value;
-        if (valueSearch) {
-            resetList()
-            displayList(`&name=${valueSearch}`)
+        if (valueSearch ) {
+            if (valueSearch === ' ') {
+                searchInput.value = ''
+                alert('Seu campo está vazio!')
+            } else {
+                resetList()
+                displayList(`&name=${valueSearch}`)
+            }
         } else {
             displayList()
         }
