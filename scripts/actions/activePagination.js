f5b87a2c3ff96e70e463eaf115bba7c15770f7e6
André Bez Birolo <andre.birolo@cayman.com.br>
2021-07-23 16:40:02 -0300

    Added arrow scope

--- scripts/actions/activePagination.js
+++ scripts/actions/activePagination.js
@@ -1,7 +1,7 @@
 // This function active the pagination items to make them clickable and realize the fetch with offset
 const activePagination = () => {
     // Get all pagination items
-    const itemsPagination = document.querySelectorAll('.pagination-container .pagination-number');
+    const itemsPagination = document.querySelectorAll('.pagination-container a');
     itemsPagination.forEach((item) => {
         let offset = item.dataset.offset
         item.addEventListener('click', () => {
--- scripts/generators/createPagination.js
+++ scripts/generators/createPagination.js
@@ -9,25 +9,25 @@ const generatePagination = (page, limit, offset, total) => {
     // Clean the list of pagination items
     resetList('pagination')
 
-
-    // if (actualPage > 2 && actualPage < 5) {
-    //     const prevAll = document.createElement('a')
-    //     prevAll.innerHTML = '<<'
-    //     prevAll.title = 'Voltar ao início'
-    //     prevAll.classList.add('back-to-first', 'font-bold', 'fast')
-    //     paginationContainer.appendChild(prevAll)
-    //     const prevOne = document.createElement('a')
-    //     prevOne.classList.add('back-one', 'font-bold', 'fast')
-    //     prevOne.innerHTML = '<'
-    //     prevOne.title = 'Voltar um'
-    //     paginationContainer.appendChild(prevOne)
-    // } else if (actualPage === (limitPagination-1)){
-    //     const prevOne = document.createElement('a')
-    //     prevOne.classList.add('back-one', 'font-bold', 'fast')
-    //     prevOne.innerHTML = '<'
-    //     prevOne.title = 'Voltar um'
-    //     paginationContainer.appendChild(prevOne)
-    // }
+    // Pagination arrows back
+    if (actualPage > 1) {
+        if (actualPage > 2) {
+            const prevAll = document.createElement('a')
+            prevAll.innerHTML = '<<'
+            prevAll.title = 'Voltar ao início'
+            prevAll.classList.add('back-to-first', 'font-bold', 'fast')
+            prevAll.dataset.offset = 0
+            prevAll.dataset.page = 1
+            paginationContainer.appendChild(prevAll)
+        }
+        const prevOne = document.createElement('a')
+        prevOne.classList.add('back-one', 'font-bold', 'fast')
+        prevOne.innerHTML = '<'
+        prevOne.title = 'Voltar um'
+        prevOne.dataset.offset = offset * actualPage
+        prevOne.dataset.page = actualPage - 1
+        paginationContainer.appendChild(prevOne)
+    }
 
 
     let start = actualPage - 2
@@ -35,11 +35,14 @@ const generatePagination = (page, limit, offset, total) => {
     if (start < 2) {
         start = 1
         finish = 5
+    } else if (actualPage >= (pageCount - 1)) {
+        finish = pageCount
     } else {
         start = actualPage - 2
         finish = actualPage + 2
     }
 
+    // Pagination items
     for (start; start <= finish; start++) {
         let skip = offset * (start - 1)
         const aContainer = document.createElement('a')
@@ -55,22 +58,24 @@ const generatePagination = (page, limit, offset, total) => {
         paginationContainer.appendChild(aContainer)
     }
 
-    // if (actualPage <= limitPagination) {
-    //     const advanceOne = document.createElement('a')
-    //     advanceOne.classList.add('advance-one', 'font-bold', 'fast')
-    //     advanceOne.innerHTML = '>'
-    //     advanceOne.title = 'Avançar um'
-    //     paginationContainer.appendChild(advanceOne)
-    //     const advanceAll = document.createElement('a')
-    //     advanceAll.innerHTML = '>>'
-    //     advanceAll.title = 'Ir para o final'
-    //     advanceAll.classList.add('advance-to-finish', 'font-bold', 'fast')
-    //     paginationContainer.appendChild(advanceAll)
-    // } else if (actualPage === (limitPagination-1)) {
-    //     const advanceOne = document.createElement('a')
-    //     advanceOne.classList.add('advance-one', 'font-bold', 'fast')
-    //     advanceOne.innerHTML = '>'
-    //     advanceOne.title = 'Avançar um'
-    //     paginationContainer.appendChild(advanceOne)
-    // }
+    // Arrows to advance
+    if (actualPage < pageCount) {
+        const advanceOne = document.createElement('a')
+        advanceOne.classList.add('advance-one', 'font-bold', 'fast')
+        advanceOne.innerHTML = '>'
+        advanceOne.title = 'Avançar um'
+        advanceOne.dataset.offset = offset * actualPage
+        advanceOne.dataset.page = actualPage + 1
+        paginationContainer.appendChild(advanceOne)
+        if (actualPage <= (pageCount - 2)) {
+            const advanceAll = document.createElement('a')
+            advanceAll.innerHTML = '>>'
+            advanceAll.title = 'Ir para o final'
+            advanceAll.classList.add('advance-to-finish', 'font-bold', 'fast')
+            advanceAll.dataset.offset = offset * pageCount
+            advanceAll.dataset.page = pageCount
+            paginationContainer.appendChild(advanceAll)
+
+        }
+    }
 }
